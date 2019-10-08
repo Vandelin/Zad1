55dbbfd1b7aa025cc702d0db1276885f2f3c2c4c
209446 <209446@edu.p.lodz.pl>
2019-10-08 17:23:27 +0200

    1sadas

---
+++ Zad1/src/main/java/com/Zad1/Zad1/Reverse.java
@@ -0,0 +1,13 @@
+package com.Zad1.Zad1;
+
+public class Reverse {
+    private final String content;
+
+    public Reverse(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
